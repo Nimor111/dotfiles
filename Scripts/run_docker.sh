05d271494970c551a946658162471aef051055c5
Nimor111 <georgi.bojinov@hotmail.com>
2017-03-20 21:28:32 +0200

    modify zshrc, add some generic scripts

---
+++ Scripts/run_docker.sh
@@ -0,0 +1,3 @@
+sudo rm /var/run/docker.pid
+sudo systemctl stop docker.service
+sudo docker daemon -s overlay &
---
+++ Scripts/send_json.sh
@@ -0,0 +1,4 @@
+curl -X POST \
+     -H "Content-Type: application/json" \
+     -d '{"input": 5}'
+     http://localhost:8000/...
